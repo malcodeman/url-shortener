5aa4fd1e672d6b5eaf33123f6da3b09549bab021
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-08-22 23:32:44 +0200

    Adds form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,64 @@
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const defaultValues = {
+  longUrl: "",
+};
+const schema = yup
+  .object({
+    longUrl: yup
+      .string()
+      .url("URL needs to be valid")
+      .required("URL is required"),
+  })
+  .required();
 
 const Home: NextPage = () => {
-  return <div>App</div>;
+  const form = useForm({ defaultValues, resolver: yupResolver(schema) });
+
+  const handleOnSubmit = (values: { longUrl: string }) => {
+    console.log(values);
+  };
+
+  return (
+    <Center minH="100vh">
+      <Container maxW="365px">
+        <Box>
+          <Heading fontSize="2xl" mb="4" textAlign="center">
+            Top TIER Shortener
+          </Heading>
+          <form onSubmit={form.handleSubmit(handleOnSubmit)}>
+            <FormControl
+              isInvalid={Boolean(form.formState.errors.longUrl)}
+              mb="2"
+            >
+              <FormLabel>Long URL</FormLabel>
+              <Input {...form.register("longUrl")} />
+              <FormHelperText>
+                {form.formState.errors.longUrl?.message}
+              </FormHelperText>
+            </FormControl>
+            <Button type="submit" width="full" colorScheme="blue">
+              Shorten it
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </Center>
+  );
 };
 
 export default Home;
